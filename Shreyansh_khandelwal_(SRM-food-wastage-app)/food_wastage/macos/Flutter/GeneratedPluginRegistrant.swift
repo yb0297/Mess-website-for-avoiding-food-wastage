2add8a1120d1b3de1e1f3012d954c2e8c35e9181
Shreyansh Khandelwal <khandelwal.happy.hk@gmail.com>
2024-04-07 02:56:54 +0530

    initial commit

---
+++ Shreyansh_khandelwal_(SRM-food-wastage-app)/food_wastage/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import firebase_core
+import flutter_avif_macos
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterAvifPlugin.register(with: registry.registrar(forPlugin: "FlutterAvifPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
